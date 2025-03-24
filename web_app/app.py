09088f96c419cff57d3598e6921ccd96e7bbe58e
LAMagicx <samsgrice@gmail.com>
2025-03-24 21:54:27 +0100

    refactor: inital work

--- web_app/app.py
+++ web_app/app.py
@@ -1,236 +1,83 @@
+import os
+from typing import Optional, List
+from pydantic import BaseModel
+
 from flask import Flask, render_template, send_file, request, url_for, redirect, session
 from flask_session import Session
 from requests import status_codes
-from utils.db import fetch_access_token, make_request, refresh_token
-from utils.schema import Page, CreatePage, Command
-from pydantic import ValidationError
-import os
-import time
+
 import hashlib
+
+from db import SurrealDB, SurrealTable
+
+SurrealDB.connect("http://localhost:8000",
+                  "root", # os.environ.get("SURREAL_USERNAME", "root"),
+                  "notroot", # os.environ.get("SURREAL_PASSWORD", "root"),
+                  "python",
+                  "database_test")
+
+class Command(BaseModel):
+    name: str
+    text: str
+    page: int | str
+    required: List[Optional[int | str]] = []
+
+class Page(SurrealTable):
+    title: str
+    text: str
+    limit: int
+    commands: List[Optional[Command]] = []
+                  
 
 app = Flask(__name__)
 
-# Configuration
-
 app.config["SESSION_TYPE"] = "cachelib"
-app.secret_key = os.environ.get("COVERSE_SECRET_KEY")
-app.config["SECRET_KEY"] = os.environ.get("COVERSE_SECRET_KEY")
-app.config["USERNAME"] = os.environ.get("COVERSE_USERNAME")
-app.config["PASSWORD"] = os.environ.get("COVERSE_PASSWORD")
-app.config["API_URL"] = os.environ.get("COVERSE_API_URL")
-app.config["JWT_SECRET_KEY"], _ = fetch_access_token(
-    app.config["API_URL"], app.config["USERNAME"], app.config["PASSWORD"]
-)
-app.config["JWT_TOKEN_LOCATION"] = ["headers"]
 Session(app)
-
-
-def get_page(page_id: int, uid: str) -> Page | None:
-    res = make_request(
-        "GET", f"/v1/pages/{page_id}", token=app.config["JWT_SECRET_KEY"]
-    )
-    if res.status_code == 200:
-        session["visits"][uid].append(page_id)
-        return Page.model_validate(res.json())
-    return None
-
-
-def post_page(page: CreatePage):
-    res = make_request(
-        "POST",
-        f"/v1/pages/{page.id}",
-        data=page.model_dump(),
-        token=app.config["JWT_SECRET_KEY"],
-    )
-    if res.status_code == 201:
-        # success
-        return redirect(f"/page/{page.id}")
-    if res.status_code == 422:
-        # unprossable entity
-        return render_template(
-            "create.html",
-            page_id=page.id,
-            error="Error in the page. Your text is probably too long.",
-            page=page,
-        )
-    elif res.status_code == 409:
-        # page exists
-        return render_template(
-            "not_found.html",
-            page_id=page.id,
-            error="The page already exists. Try to create a different page.",
-        )
-    else:
-        # other error
-        return render_template("404.html"), res.status_code
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    print(f"404: {e}")
     return render_template("404.html"), 404
-
 
 @app.before_request
 def check_token():
+    print("here")
+    print(session)
     if "visits" not in session:
         session["visits"] = {}
-    app.config["JWT_SECRET_KEY"] = refresh_token(
-        app.config["JWT_SECRET_KEY"],
-        app.config["API_URL"],
-        app.config["USERNAME"],
-        app.config["PASSWORD"],
-    )
+
+    if "user_id" not in session.keys():
+        ua = request.headers.get("User-Agent")
+        ip = request.access_route[-1]
+        uid = f"{ip}_{hashlib.sha1(ua.encode('utf-8')).hexdigest()[:8]}"
+        print(f"New user: {uid}")
+        session["user_id"] = uid
 
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
-@app.route("/page/<int:page_id>")
+@app.route("/page/<int:page_id>", methods=["GET"])
 def show_page(page_id: int):
-    # manage session
-    if "user-id" in session:
-        uid = session["user-id"]
-    else:
-        ua = request.headers.get("User-Agent")
-        ip = request.access_route[-1]
-        uid = f"{ip}_{hashlib.sha1(ua.encode('utf-8')).hexdigest()[:8]}"
-        print(f"New user: {uid}")
-        session["user-id"] = uid
+    user_id = session['user_id']
+    if user_id not in session["visits"]:
+        session["visits"][user_id] = [] 
 
-    if uid not in session["visits"]:
-        session["visits"][uid] = []
+    session["visits"][user_id].append(page_id)
 
-    prev_command_text = request.args.get("command", default="", type=str)
-    page = get_page(page_id=page_id, uid=uid)
-    if page is not None:
-        allowed_commands = [
-            set([p[5:] for p in r]).issubset(session["visits"][uid])
-            for r in page.commands.required
-        ]
-        if prev_command_text != "":
-            return render_template(
-                "page.html",
-                page=page,
-                command_text=prev_command_text,
-                allowed=allowed_commands,
-                zip=zip,
-            )
-        else:
-            return render_template(
-                "page.html", page=page, allowed=allowed_commands, zip=zip
-            )
-    else:
-        return render_template("not_found.html", page_id=page_id)
+    page = Page.select(page_id)
+    if page is None:
+        return render_template('not_found.html')
+
+    return render_template('page.html', page=page)
 
 
 @app.route("/create/<int:page_id>", methods=["GET", "POST"])
-def create(page_id: int):
-    if request.method == "POST":
-        form_data = request.form.to_dict(flat=False)
-
-        # {'id': ['0'], 'title': ['werew'], 'text': ['qwrqwr'], 'limit': ['2'], 'commands[0][name]': ['qwrqwr'], 'commands[0][text]': ['qwrqwr'], 'commands[0][page]': ['1'], 'commands[0][required]': [''], 'commands[1][name]': ['qwrqwr'], 'commands[1][text]': ['qwrqrw'], 'commands[1][page]': ['2'], 'commands[1][required]': ['']}
-        # Parse commands from form data
-        print(form_data)
-        commands = []
-        for i in range(int(form_data.get("limit", ["0"])[0])):
-            print(i)
-            if f"commands[{i}][name]" not in form_data.keys():
-                break
-            try:
-                command = Command(
-                    name=form_data[f"commands[{i}][name]"][0],
-                    text=form_data[f"commands[{i}][text]"][0],
-                    page=int(form_data[f"commands[{i}][page]"][0]),
-                    required=[
-                        int(req.strip())
-                        for req in form_data.get(f"commands[{i}][required]", [""])[
-                            0
-                        ].split(",")
-                        if req.strip().isdecimal()
-                    ],
-                )
-                commands.append(command)
-            except ValidationError as e:
-                print(f"Error in command {i + 1}: {e}", "error")
-                return render_template("create.html", page_id=page_id, error=e)
-
-        try:
-            page = CreatePage(
-                id=page_id,
-                title=form_data["title"][0],
-                text=form_data["text"][0],
-                limit=int(form_data["limit"][0]),
-                commands=commands,
-            )
-            print(page)
-            return post_page(page)
-        except ValidationError as e:
-            print(f"Error in page data: {e}", "error")
-            return render_template("create.html", page_id=page_id, error=e)
-
-    return render_template("create.html", page_id=page_id)
-
-
-@app.route("/config", methods=["GET", "POST"])
-def update_config():
-    if request.method == "GET":
-        return render_template(
-            "config.html",
-            api_url=app.config["API_URL"],
-            username=app.config["USERNAME"],
-        )
-    elif request.method == "POST":
-        api_url = request.form.get("url")
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        # Validation to ensure fields are not empty
-        if not api_url or not username or not password:
-            return render_template(
-                "config.html",
-                api_url=app.config["API_URL"],
-                username=app.config["USERNAME"],
-                error="All fields are required",
-            )
-        app.config["USERNAME"] = username
-        app.config["PASSWORD"] = password
-        app.config["API_URL"] = api_url
-        try:
-            new_token, status_code = fetch_access_token(
-                app.config["API_URL"], app.config["USERNAME"], app.config["PASSWORD"]
-            )
-        except Exception as e:
-            print(e)
-            return render_template(
-                "config.html",
-                api_url=app.config["API_URL"],
-                username=app.config["USERNAME"],
-                error="Could not connect to server",
-            )
-        if status_code != 200:
-            return render_template(
-                "config.html",
-                api_url=app.config["API_URL"],
-                username=app.config["USERNAME"],
-                error="username or password is incorrect",
-            )
-        elif new_token is not None:
-            app.config["JWT_SECRET_KEY"] = new_token
-            app.config["JWT_TOKEN_LOCATION"] = ["headers"]
-            return redirect("/")
-
-
-@app.route("/sw.js")
-def serve_sw():
-    return send_file("static/sw.js", mimetype="application/javascript")
-
-
-@app.route("/manifest.json")
-def serve_manifest():
-    return send_file("static/manifest.json", mimetype="application/manifest+json")
+def create_page(page_id: int):
 
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=8085)
---
+++ web_app/db.py
@@ -0,0 +1,80 @@
+from pydantic_core import core_schema
+from pydantic import BaseModel, GetCoreSchemaHandler, field_validator
+from typing import Any
+from surrealdb import Surreal, RecordID
+
+class ID(RecordID):
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source: type[Any], handler: GetCoreSchemaHandler
+    ) -> core_schema.CoreSchema:
+        def validate(value: Any, info: core_schema.ValidationInfo) -> ID:
+            if isinstance(value, RecordID):
+                return ID(table_name=value.table_name, identifier=value.id)
+
+            if info.config is not None and "title" not in info.config.keys():
+                print(info)
+                raise ValueError("Title of root model not found")
+            
+            return ID(table_name=info.config["title"].lower(), identifier=value)
+        return core_schema.with_info_after_validator_function(
+            validate,
+            core_schema.int_schema()
+        )
+
+class SurrealDB:
+    _db: Any
+
+    @classmethod
+    def connect(cls, url: str, username: str, password: str, namespace: str, database: str):
+        cls._db = Surreal(url)
+        cls._db.signin({"username": username, "password": password})
+        cls._db.use(namespace, database)
+
+    @classmethod
+    def get_db(cls) -> Any:
+        if cls._db is None:
+            raise ValueError("Database not connected yet. Call SurrealDB.connect(...) first.")
+        return cls._db
+
+class SurrealTable(BaseModel):
+    id: ID
+
+    @classmethod
+    def table_name(cls) -> str:
+        return cls.__name__.lower()
+
+    @field_validator('id', mode='before')
+    @classmethod
+    def handle_record_id_input(cls, value: Any) -> int:
+        if isinstance(value, RecordID):
+            return value.id
+        return value
+
+    @classmethod
+    def select(cls, id: str | int):
+        db = SurrealDB.get_db()
+        result = db.select(ID(cls.table_name(), id))
+        if result:
+            return cls(**result)
+        return None
+
+    def create(self):
+        db = SurrealDB.get_db()
+        db.create(self.table_name(), self.model_dump())
+
+    def update(self):
+        db = SurrealDB.get_db()
+        if not self.id:
+            raise ValueError("Cannot update without ID")
+        db.update(self.id, self.model_dump(exclude={"id"}))
+
+    def delete(self):
+        db = SurrealDB.get_db()
+        if not self.id:
+            raise ValueError("Cannot delete without ID")
+        db.delete(self.id)
+
+
+if __name__ == "__main__":
+    SurrealDB.connect("http://127.0.0.1:8000", "root", "notroot", "python", "database_test")
